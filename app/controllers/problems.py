aacc0c6bd0211070b3000871b7277f0d74ebde45
ryutaro-kimura <sasuker2264@gmail.com>
2021-06-20 01:30:54 +0900

    [modify] #9 Get editor text

--- app/controllers/problems.py
+++ app/controllers/problems.py
@@ -1,14 +1,17 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request
 
 bp = Blueprint('problems', __name__)
+app = Flask(__name__)
 
 @bp.route("/")
 def index():
   problems = {1:"hoge1", 2:"hoge2"}
   return render_template('index.html', problems=problems)
   
-@bp.route("/<int:id>/promblem")
+@bp.route("/<int:id>/promblem", methods=['GET', 'POST'])
 def problem(id):
+  if request.method == 'POST':
+    code = request.form['code']
   problems = {1:"hoge1", 2:"hoge2"}
   return render_template('problem.html', problem=problems[id])
 
